0a8ec327d3674da156949dc36aea5d2958c14038
Luciano <luciano.carrizo26@gmail.com>
2024-07-22 22:15:17 -0300

    Add: json realms and view

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,9 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+
+const filePath = path.join(__dirname, '../data/realms.json')
+
 module.exports = async (req, res) => {
     try {
+        const realms = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
 
-        return res.render('index')
+        return res.render('index', {
+            realms
+        })
 
     } catch (error) {
         console.log(error);
     }
 }
+
+// demonio "https://ts2.mm.bing.net/th?q=tribal%20angel",
