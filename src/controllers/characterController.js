c296166c92c4e200d386547ab7b4b3663398a747
Luciano <luciano.carrizo26@gmail.com>
2024-07-23 20:01:29 -0300

    Add: edit

--- src/controllers/characterController.js
+++ src/controllers/characterController.js
@@ -1,6 +1,8 @@
 module.exports = {
   all: require('./character/all'),
+  detail: require('./character/detail'),
   add: require('./character/add'),
   create: require('./character/create'),
   modify: require('./character/modify'),
+  edit: require('./character/edit'),
 }
--- src/routers/character.Routes.js
+++ src/routers/character.Routes.js
@@ -1,17 +1,18 @@
 const express = require('express')
-const {add, modify, all, create} = require('../controllers/characterController')
+const {add, modify, all, create, edit, detail} = require('../controllers/characterController')
 const router = express.Router()
 const upload = require('../middleWares/upload')
 
 
 //detalles
 router.get('/all', all)
+router.get('/detail/:id', detail)
 //crear
 router.get('/add', add)
 router.post('/add', upload.single('image'), create)
 //editar
 router.get('/edit/:id', modify)
-
+router.put('/edit/:id', upload.single('image'), edit)
 //borrar
 
 module.exports = router
