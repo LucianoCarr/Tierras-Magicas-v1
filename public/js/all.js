08f06535b75066d87ce17f069f1ce0dcec707440
Luciano <luciano.carrizo26@gmail.com>
2024-07-26 20:49:16 -0300

    css header & footer done

--- public/js/all.js
+++ public/js/all.js
@@ -10,14 +10,14 @@ window.onload = function() {
             descripcion.style.maxHeight = descripcion.scrollHeight + 'px';
             boton.innerHTML = 'Ver menos ↑';
           } else {
-            descripcion.style.maxHeight = '5px'; // Ajusta este valor según tu preferencia
+            descripcion.style.maxHeight = '0px'; // Ajusta este valor según tu preferencia
             boton.innerHTML = 'Ver más ↓';
           }
       });
   
       // Inicialmente, colapsamos la descripción
       var descripcion = boton.parentNode.querySelector('.card-text');
-      descripcion.style.maxHeight = '5px'; // Ajusta este valor según tu preferencia
+      descripcion.style.maxHeight = '0px'; // Ajusta este valor según tu preferencia
       descripcion.style.overflow = 'hidden';
       descripcion.style.transition = 'max-height 0.3s ease-out';
     });
